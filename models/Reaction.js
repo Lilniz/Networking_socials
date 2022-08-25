b273beb2ced60c0d1e599a07122e2e615e250927
Lizzle <leefboettcher@gmail.com>
2022-08-25 01:25:39 -0500

    Initial commit

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { ObjectId } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const { Reaction } = require('.');
+
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            timestamps: {
+                currentTime: () => Math.floor(Date.now() / 1000)
+              }
+        }
+    }
+);
+
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction;
---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction.js')
+
+// Schema to create a thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000)
+      }
+    },
+    userName: {
+      type: String,
+      required: true
+    },
+    reactions: [
+      reactionSchema
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
